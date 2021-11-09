38a00406122d8a896ca672c527fb54cd7e396bb0
mike <apolloproductionsdj@gmail.com>
2021-11-09 12:11:54 -0700

    added in firebase

--- App.js
+++ App.js
@@ -3,13 +3,17 @@ import { StyleSheet, Text, View } from 'react-native';
 import tw from 'tailwind-rn';
 import StackNavigator from './StackNavigator';
 import { NavigationContainer } from '@react-navigation/native';
+import { AuthProvider } from './hooks/useAuth';
 
 
 
 export default function App() {
   return (
+
     <NavigationContainer>
-      <StackNavigator />
+      <AuthProvider>
+        <StackNavigator />
+      </AuthProvider>
     </NavigationContainer>
   );
 }
